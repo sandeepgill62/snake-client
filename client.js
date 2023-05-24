e619da88cd731d8bc4abc342579c77e7f0db7db3
Sandeep Gill <sandeepgill62@gmail.com>
2023-05-24 08:58:44 +0000

    move connect function from play.js to client.js

---
+++ client.js
@@ -0,0 +1,26 @@
+// connect.js
+
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost", // IP address here,
+    port: 50541 // PORT number here,
+  });
+
+  conn.on("data", (data) => {
+    console.log("Server says: ", data);
+  });
+
+  conn.on("connect", () => {
+    conn.write("Hello from client!");
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+module.exports = { connect };
--- play.js
+++ play.js
@@ -1,58 +1,6 @@
-// const net = require("net");
+// play.js
 
-// // establishes a connection with the game server
-// const connect = function () {
-//   const conn = net.createConnection({
-//     host: "10.0.2.15", // IP address here,
-//     port: 50541 // PORT number here,
-//   });
-
-
-//   conn.on("data", (data) => {
-//     console.log("Server says: ", data);
-//   });
-
-//   conn.on("connect", () => {
-//     conn.write("Hello from client!");
-//   });
-
-//   // interpret incoming data as text
-//   conn.setEncoding("utf8");
-
-
-
-//   return conn;
-// };
-
-// console.log("Connecting ...");
-
-// connect();
-
-
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost", // IP address here,
-    port: 50541 // PORT number here,
-  });
-
-  conn.on("data", (data) => {
-    console.log("Server says: ", data);
-  });
-
-  conn.on("connect", () => {
-    conn.write("Hello from client!");
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-
-
-  return conn;
-};
+const { connect } = require("./client.js");
 
 console.log("Connecting ...");
 connect();
