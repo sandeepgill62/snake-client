02dc89692da847f054ade906fbd920793c74840e
Sandeep Gill <sandeepgill62@gmail.com>
2023-05-24 20:23:36 +0000

    keyboard event handler

--- client.js
+++ client.js
@@ -40,5 +40,21 @@ const connect = function () {
   return conn;
 };
 
+// setup interface to handle user input from stdin
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+const handleUserInput = function () {
+  console.log("ok");
+  // your code here
+};
 
 module.exports = { connect };
